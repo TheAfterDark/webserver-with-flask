d6373cc360c1ebc31ba1427128c73b019a76d428
TheAfterDark <stkrieger005@gmail.com>
2022-01-20 13:08:07 +0000

    new-route

--- run.py
+++ run.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template("index.html", data="data")
 
+@app.route("/uwu")
+def uwu():
+    return render_template("uwu.html", data="data")
+
 if __name__ == "__main__":
     app.run()
